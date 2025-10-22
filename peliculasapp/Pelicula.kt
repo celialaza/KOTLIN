246f34895fa5115b1702bb361c21f5b267db10c6
celialaza <celia.s234913@cesurformacion.com>
2025-10-22 12:40:36 +0200

    Delete peliculasapp directory

--- peliculasapp/Pelicula.kt
+++
@@ -1,5 +0,0 @@
-package com.example.peliculasapp
-
-data class Pelicula(val id: Int, val titulo: String, val descripcion: String, val imagen: Int, val duracion:Int, val año:Int, val pais:String) {
-
-}
